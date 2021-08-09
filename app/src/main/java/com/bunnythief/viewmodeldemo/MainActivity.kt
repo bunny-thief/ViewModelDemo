028f8fd5e1d391b3d0bf7c8daea421dd60414bd1
Jacques Navarro <jacques.navarro@protonmail.com>
2021-08-09 07:30:18 -0400

    Add data binding, buttons, logic

--- app/src/main/java/com/bunnythief/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/bunnythief/viewmodeldemo/MainActivity.kt
@@ -2,10 +2,37 @@ package com.bunnythief.viewmodeldemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.bunnythief.viewmodeldemo.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    //add reference to binding object
+    private lateinit var binding: ActivityMainBinding
+
+    var randomInt: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //create binding object
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.randomButton.setOnClickListener {
+        binding.textView.text = setRandomInt().toString()
+        }
+
+        binding.resetButton.setOnClickListener {
+            resetIntText()
+        }
+    }
+
+    private fun setRandomInt(): Int {
+        return Random.nextInt(1, 10)
+    }
+
+    private fun resetIntText() {
+        binding.textView.text = "Click button"
     }
 }
