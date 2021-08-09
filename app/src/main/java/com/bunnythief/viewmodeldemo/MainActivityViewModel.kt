768809c5b036658f6db791868f9ac6d97bb5c960
Jacques Navarro <jacques.navarro@protonmail.com>
2021-08-09 07:34:43 -0400

    Add MainActivityViewModel class

---
+++ app/src/main/java/com/bunnythief/viewmodeldemo/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.bunnythief.viewmodeldemo
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+    init {
+        Log.i("MainActivityViewModel", "MainActivityViewModel created!")
+    }
+
+}
